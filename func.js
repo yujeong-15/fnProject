063c2f1cd3546b5140fc5a73ff4c7a1a40192d43
yujoneg-15 <codbwjd1@naver.com>
2024-06-21 15:06:39 +0900

    fn-server만들기

---
+++ func.js
@@ -0,0 +1,10 @@
+const fdk = require("@fnproject/fdk");
+
+fdk.handle(function (input) {
+  let name = "World";
+  if (input.name) {
+    name = input.name;
+  }
+  console.log("\nInside Node Hello World function");
+  return { message: "Hello " + name };
+});
